5b8d551200d8c215a91d7f927101b2609bd0cba1
felovilches <felovilches@gmail.com>
2016-11-13 00:21:48 -0300

    puse las imagenes

--- build/generated-sources/ap-source-output/entities/Producto_.java
+++ build/generated-sources/ap-source-output/entities/Producto_.java
@@ -4,17 +4,17 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-12T22:16:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-12T23:57:51")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 
-    public static volatile SingularAttribute<Producto, String> descripcion;
-    public static volatile SingularAttribute<Producto, String> nombreVendedor;
-    public static volatile SingularAttribute<Producto, Integer> precio;
-    public static volatile SingularAttribute<Producto, String> ciudad;
     public static volatile SingularAttribute<Producto, Long> id;
     public static volatile SingularAttribute<Producto, String> nombre;
     public static volatile SingularAttribute<Producto, String> imagenUrl;
+    public static volatile SingularAttribute<Producto, String> ciudad;
     public static volatile SingularAttribute<Producto, String> numeroTelefono;
+    public static volatile SingularAttribute<Producto, Integer> precio;
+    public static volatile SingularAttribute<Producto, String> descripcion;
+    public static volatile SingularAttribute<Producto, String> nombreVendedor;
 
 }
